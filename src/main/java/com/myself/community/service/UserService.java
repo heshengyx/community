4da093c29606a867ef7e88429b6971cf879bc546
hes <heshengyx@qq.com>
2015-04-23 09:36:20 +0800

    9

--- src/main/java/com/myself/community/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.myself.community.service;
-
-import com.myself.community.entity.User;
-
-public interface UserService {
-
-	int save(User user);
-	User getUserByAccount(String account);
-}
